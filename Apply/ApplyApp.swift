c023691e283c097ad2d30b14799dfa51e28982a3
Kedar Abhyankar <krabhyankar@gmail.com>
2021-12-24 14:56:16 -0800

    Initial Commit

---
+++ Apply/ApplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplyApp.swift
+//  Apply
+//
+//  Created by Kedar Abhyankar on 12/24/21.
+//
+
+import SwiftUI
+
+@main
+struct ApplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apply/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apply
+//
+//  Created by Kedar Abhyankar on 12/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
